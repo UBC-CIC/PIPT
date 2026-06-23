832eeae1e05c450dae4ab4c112b697e5514d6802
Rajrupa Sanyal <rajrupasanyal@gmail.com>
2026-06-23 14:02:54 -0700

    deleted old migrations

--- cdk/lambda/db_setup/migrations/011_voice_enabled.js
+++
@@ -1,7 +0,0 @@
-exports.up = (pgm) => {
-  pgm.sql(`ALTER TABLE personas ADD COLUMN IF NOT EXISTS voice_enabled boolean DEFAULT true`);
-};
-
-exports.down = (pgm) => {
-  pgm.sql(`ALTER TABLE personas DROP COLUMN IF EXISTS voice_enabled`);
-};
